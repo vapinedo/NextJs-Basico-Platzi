f750ec8a9059aaebca88ecd4bbfd2f3a86b56b56
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-03-27 13:18:43 -0500

    [ADD] creando y consumiendo nuestra API

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from '@components/Navbar/Navbar'
+
+const apiUrl = '/api/avo'
+
+const Home = () => {
+
+  const [productList, setProductList] = React.useState<TProduct[]>([])
+
+  React.useEffect(() => {
+    window.fetch(apiUrl)
+      .then(response => response.json())
+      .then(({ data, dataLength }) => setProductList(data))
+  }, []);
+  
+  return (
+    <div>
+      <Navbar />
+      <h1>Home Page</h1>
+
+      {productList.map(product => (
+        <div key={product.id}>{product.name}</div>
+      ))}
+    </div>
+  )
+}
+
+export default Home
