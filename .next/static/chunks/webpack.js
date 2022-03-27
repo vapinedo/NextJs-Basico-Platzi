398c9ccf3d6e103f8046940998a87c96c47f34d3
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-03-27 16:08:55 -0500

    [ADD] extendiendo el document

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "58feab631196eef0"; }
+/******/ 		__webpack_require__.h = function() { return "74c48cafb371064e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
